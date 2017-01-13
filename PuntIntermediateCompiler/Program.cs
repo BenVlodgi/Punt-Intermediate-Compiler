6e54dba31f23abf6988e70c5528ea4246a3dc0ae
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2017-01-13 01:01:42 -0500

    Add coop instance swapping

--- PuntIntermediateCompiler/Program.cs
+++ PuntIntermediateCompiler/Program.cs
@@ -40,7 +40,45 @@ namespace PuntIntermediateCompiler
             entities = vmf.Body.Where(item => item.Name == "entity").Select(item => item as VBlock).ToList();
             instances = entities.Where(entity => entity.Body.Where(item => item.Name == "classname" && (item as VProperty).Value == "func_instance").Count() > 0).ToList();
             flags = instances.Where(instance => instance.Body.Where(item => item.Name == "targetname" && (item as VProperty).Value.StartsWith("PuzzleMakerFlag_")).Count() > 0).ToList();
-            
+
+            hasChanged = Mod_COOPChanges() || hasChanged;
+
+            return hasChanged;
+        }
+
+        internal static bool Mod_COOPChanges()
+        {
+            bool hasChanged = false;
+
+            //If is coop, add point entity where the elevator instance is.
+            var coop_exit = instances.Where(instance =>
+                instance.Body.Where(property =>
+                    property.Name == "file" &&
+                    property.GetType() == typeof(VProperty) &&
+                    ((VProperty)property).Value.EndsWith("coop_exit.vmf"))
+                    .Count() == 1).FirstOrDefault();
+            if (coop_exit != null)
+            {
+                //Then this must be coop
+                var coop_exit_origin = coop_exit.Body.Where(property => property.Name == "origin" && property.GetType() == typeof(VProperty)).FirstOrDefault() as VProperty;
+                if (coop_exit_origin == null)
+                {
+                    Console.WriteLine("We have a coop exit, with no origin?");
+                    return false;
+                }
+
+                #region Swap all singleplayer instances for coop instances.
+                foreach (var instance in instances)
+                {
+                    var file = instance.Body.FirstOrDefault(property => property.GetType() == typeof(VProperty) && property.Name == "file") as VProperty;
+                    if (file.Value.EndsWith("_sp.vmf"))
+                        file.Value = file.Value.Replace("_sp.vmf", "_coop.vmf");
+                }
+                #endregion
+
+                hasChanged = true;
+            }
+
             return hasChanged;
         }
     }
